c0c702655c42f303679cb45291d16055869a7f6c
renatossanches <renato.sanches2004@gmail.com>
2025-05-05 13:22:07 -0300

    Refactor: melhorias em retorno de mensagem de erro e layout

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,3 +1,38 @@
+    <!-- Navbar -->
+    <nav class="dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+        <div class="max-w-7xl mx-auto px-4 py-4 flex justify-between items-center">
+            <div class="text-xl font-bold text-gray-800"> 
+            <a href="{{ route('dashboard') }}" class="text-gray-600 hover:text-gray-900 mr-4">
+            <img src="{{ asset('/foto-gacbank.png') }}" alt="Logo" class="w-[60px] h-[60px]"/>
+            </a>
+            </div>
+
+            <div class="hidden sm:flex sm:items-center sm:ms-6">
+            <x-dropdown align="right" width="48">
+            <x-slot name="trigger">
+                <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 dark:text-gray-400 bg-white dark:bg-gray-800 hover:text-gray-700 dark:hover:text-gray-300 focus:outline-none transition ease-in-out duration-150">
+                    <div class="ms-1">
+                        <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                            <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                        </svg>
+                    </div>
+                </button>
+            </x-slot>
+
+            <x-slot name="content">
+                <x-dropdown-link :href="route('login')">
+                    {{ __('Login') }}
+                </x-dropdown-link>
+                <x-dropdown-link :href="route('register')">
+                    {{ __('Registro') }}
+                </x-dropdown-link>
+            </x-slot>
+            </x-dropdown>
+
+
+            </div>
+        </div>
+    </nav>
 <x-guest-layout>
     <form method="POST" action="{{ route('register') }}">
         @csrf
